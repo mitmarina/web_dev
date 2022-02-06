3f84c22ec0e740489c3eb0e2b41d37e882942699
Marina <marina_voroshkevich@icloud.com>
2022-02-06 18:30:43 +0300

    New title

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+let btn = $('#btn_send');
+console.log(btn);
+
+let inputFormMail = $('input')[1];
+console.log(inputFormMail);
+
+let inputFormSubject = $('input')[2];
+console.log(inputFormSubject);
+
+
+
+btn.click(function() {
+    if(!value) {
+        return
+    } 
+})
+
+
+
+})
